2e7dab77f50fc35ff8bae635917f7f39cb1a2375
maloraen <158532343+maloraen@users.noreply.github.com>
2025-03-28 14:23:24 -0500

    add logic for button behaviors and display updating
    - used math.js to test that the buttons and display behave as expected;
      will replace with custom operation functions later
    - used button.classList.contains() to differentiate button types depending
      on their classes (.number, .operator, .equals, etc)
    - added logic to track whether the display is showing a result using a
      `.result` class that is added or removed with DOM methods in the button
      event listeners
    
    - calculator currently:
      - updates display when a number or an operator button is clicked
      - shows the result of the expression when the equals button is clicked
      - clears the result from the display if another number is clicked, or
        appends an operator if an operator button is clicked
      - evaluates more than a single pair of numbers at a time; will change
        the logic to meet assignment specifications later
    
    note: i decided to work on the layout of the calculator first because i
          only had enough time to do so when i started the project. i then
          chose to work on the basic funcionality of the buttons to test
          interactivity, as well as to set up the foundation for future logic

--- script.js
+++ script.js
@@ -19,10 +19,45 @@ function operate() {
 }
 */
 
+const display = document.querySelector(".display span");
 const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
     let char = button.textContent;
     button.addEventListener("click", () => {
-        console.log(`the ${char} button was clicked`);
+        console.log(`${char}`);
+        if (button.classList.contains("number")) {
+            console.log("a number button was clicked");
+            if (display.textContent == 0) {
+                display.textContent = char;
+            } else if (display.classList.contains("result")) {
+                display.classList.remove("result");
+                display.textContent = char;
+            }
+            else {
+                display.textContent += char;
+            }
+        }
+        if (button.classList.contains("operator")) {
+            console.log("an operator button was clicked");
+            display.textContent += char;
+            if (display.classList.contains("result")) {
+                display.classList.remove("result");
+            }
+        }
+        if (button.classList.contains("equals")) {
+            console.log("the equal button was clicked");
+            let result = math.evaluate(display.textContent);
+            console.log(`result: ${result}`)
+            display.classList.add("result");
+            display.textContent = result;
+        }
     })
 });
+
+
+// when number button clicked, update display to show number
+// when opertor button clicked, update display to show number and operator
+// when another button clicked, update display to show number operator number
+// when equals button clicked, run calculation, update display to show result (and last operation above it ?)
+  // if another number is clicked, clear result, go back to step 1
+  // if another operator is clicked, show operator after result (and clear last operation), go back to step 3
